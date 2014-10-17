96909169e5d47317e7a778293b256c35b8683718
Robert Wiener <robert.wiener@abilitynetwork.com>
2014-10-17 09:47:35 -0400

    New UnitTest Project

---
+++ Acme.Portal/Acme.Portal.Test/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Web.Mvc;
+using Acme.Portal.Web.Controllers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.Portal.Test
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var controller = new HomeController();
+            var result = controller.Index() as ViewResult;
+            Assert.AreEqual("Index", result.ViewName);
+
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var controller = new HomeController();
+            var result = controller.Edit(3) as ViewResult;
+            Assert.AreEqual("Edit", result.ViewName);
+
+        }
+    }
+}
